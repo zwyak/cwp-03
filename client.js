a804150bcdbcd001faa5094a310012f10affe1c8
zwyak <vlad.senyuk97@gmail.com>
2020-02-11 12:57:56 +0300

    Added sending files to server.js

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 // client.js
 const net = require('net');
 const fs = require('fs');
+const path = require('path');
 const port = 8124;
 const firstRequestStr = 'FILES';
 const successReq = 'ASC';
 const failedReq = 'DEC';
 
 let args = [];
+let currentPath;
 
 for (var i = 2; i < process.argv.length; i++) {
   args.push(process.argv[i]);
@@ -30,19 +32,34 @@ client.on('data', function(data) {
     console.log(data);
     for (var i = 0; i < args.length; i++) {
       if (fs.existsSync(args[i])){
+        currentPath = args[i];
         fs.readdir(args[i], function(err, list) {
-          for (var i = 0; i < list.length; i++) {
-            if ( fs.lstatSync(list[i]).isFile() ){
-              const buf = Buffer.from(list[i]);
-              client.write(buf);
+          for (var j = 0; j < list.length; j++) {
+            if ( path.extname(list[j]) == '.js' ){
+              console.log(currentPath + list[j]);
+              let sync = fs.createReadStream(path.join(currentPath + list[j]) || defaults.SYNC_FILE);
+
+              sync.on('error', function(e) {
+                console.error(e);
+              });
+
+              sync.on('open', function() {
+                sync.pipe(client);
+              });
+
+              sync.on('finish', function() {
+                console.log('finish');
+              });
             }
           }
         });
       }
     }
-}else if ( (data != successReq) && (client.RequestNumber == 1) ){
+  }else if ( (data != successReq) && (client.RequestNumber == 1) ){
     console.log(data);
     client.destroy();
+  }else{
+    //
   }
 });
 
